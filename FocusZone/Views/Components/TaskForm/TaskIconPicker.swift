6e51c23a0983a735fc34fe3c663185eeaf75a71c
julio <julio.fils1@gmail.com>
2025-07-14 14:44:59 -0400

    Add color assets for background and card, and implement TaskIconPicker component

---
+++ FocusZone/Views/Components/TaskForm/TaskIconPicker.swift
@@ -0,0 +1,18 @@
+//
+//  TaskIconPicker.swift
+//  FocusZone
+//
+//  Created by Julio J Fils on 7/14/25.
+//
+
+import SwiftUI
+
+struct TaskIconPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskIconPicker()
+}
