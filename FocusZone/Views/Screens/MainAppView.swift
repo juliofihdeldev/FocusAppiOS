7fd86a4d993e4c5643302b25c7c2f37e06aae2c3
julio <julio.fils1@gmail.com>
2025-08-30 11:33:00 -0400

    refactor: Remove all translation/localization code to restore app functionality

--- FocusZone/Views/Screens/MainAppView.swift
+++ FocusZone/Views/Screens/MainAppView.swift
@@ -4,17 +4,13 @@ struct MainAppView: View {
     @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding: Bool = false
     
     var body: some View {
-        // Temporarily show TestTranslationView for debugging
-        TestTranslationView()
-        
-        // Original code (commented out for now):
-        // Group {
-        //     if hasSeenOnboarding {
-        //         SplashScreen()
-        //     } else {
-        //         OnboardingView()
-        //     }
-        // }
+        Group {
+            if hasSeenOnboarding {
+                SplashScreen()
+            } else {
+                OnboardingView()
+            }
+        }
     }
 }
 
